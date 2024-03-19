c2f26b2883b8407cfb546234280a8fb6b1e38c63
tabed23 <tabeed68@gmail.com>
2024-03-19 13:20:25 +0500

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from moviepy.editor import VideoFileClip
-from IPython import  display
-input_video = "pexels-alexander-jensen-20496059 (1080p).mp4"
-output_video = './pexels-alexander-jensen-20496059.mp4'
+input_video = "./videos/__temp__.mp4"
+output_video = './videocompress/__temp__.mp4'
 
 clip = VideoFileClip(input_video)
 
 target_resolution = (1280, 720)
-clip_resized = clip.resize(newsize=target_resolution)
+# 60 % reducing the video
+
+clip_resized = clip.resize(0.6)
 
 clip_resized.write_videofile(output_video)
